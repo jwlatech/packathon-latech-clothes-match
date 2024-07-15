1853c6f2b7377f65c4bc7d76ae6c2cd0bd2562b5
Juan Willink <juanwillink@latechfactory.com>
2024-07-15 15:43:47 -0400

    forced deploy

--- app/sections/MatchExperience/MatchExperience.tsx
+++ app/sections/MatchExperience/MatchExperience.tsx
@@ -3,8 +3,6 @@ import {Container} from '~/components/Container';
 import type {MatchExperienceCms} from './MatchExperience.types';
 
 export function MatchExperience({cms}: {cms: MatchExperienceCms}) {
-  const {preselects} = cms;
-
   return (
     <Container container={cms.container}>
       <div>Match Experience</div>
